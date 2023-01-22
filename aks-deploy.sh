4ed0ca5645a3d4c0ce671432e3e396bade6b0871
Yongkang He <he@yongkang.cloud>
2023-01-22 20:58:48 +0800

    Update aks-deploy.sh

--- aks-deploy.sh
+++ aks-deploy.sh
@@ -9,10 +9,10 @@ az aks create \
   --location $MY_LOCATION \
   --generate-ssh-keys \
   --kubernetes-version $AKS_K8S_VERSION \
-  --node-count 2 \
+  --node-count 1 \
   --node-vm-size $MY_VMSIZE \
   --enable-cluster-autoscaler \
-  --min-count 2 \
+  --min-count 1 \
   --max-count 3 \
   --network-plugin azure
 
