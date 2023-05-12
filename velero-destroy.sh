213ad5dbcafca3bc2b48534b3fd6db01796d0ec7
Yongkang He <he@yongkang.cloud>
2023-05-12 20:00:21 +1000

    Update velero-destroy.sh

--- velero-destroy.sh
+++ velero-destroy.sh
@@ -1,8 +1,10 @@
-echo '-------Remove Velero for AKS and Clean up (typically in 3 mins)'
+echo '-------Remove Velero for AKS and Clean up (typically in ~1 min)'
 starttime=$(date +%s)
 . ./setenv.sh
 
 velero uninstall --force
+AKS_RG=$(az group list -o table | grep $MY_PREFIX-$MY_GROUP | grep MC | awk '{print $1}')
+az storage account delete -n $MY_PREFIX$AZURE_STORAGE_ACCOUNT_ID -g $AKS_RG -y
 
 endtime=$(date +%s)
 duration=$(( $endtime - $starttime ))
