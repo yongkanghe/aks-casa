810be10279c5cb1357d8a21be6c2f9f29ee97712
Yongkang He <he@yongkang.cloud>
2023-05-12 16:54:20 +1000

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -7,9 +7,9 @@ export MY_LOCATION=eastus          #Customize your location
 export MY_REGION="East US"         #Customize region for Blob Storage
 # export MY_LOCATION=centralindia     #Customize your location
 # export MY_REGION="Central India"    #Customize region for Blob Storage
-# export MY_CONTAINER=k10container4yong1      #Customize your container
+export MY_CONTAINER=velero4yong1      #Customize your container
 # export MY_OBJECT_STORAGE_PROFILE=myazblob1  #Customize your profile name
-# export AZURE_STORAGE_ACCOUNT_ID=azsa4yong1  #Customize your Storage Account
+export AZURE_STORAGE_ACCOUNT_ID=azsa4yong1  #Customize your Storage Account
 export K8S_VERSION=1.26                     #Customize your Kubernetes Version
 export MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
 
