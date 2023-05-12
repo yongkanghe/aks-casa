928e5515b5257ff85d69f5f16c5f861bfa550b11
Yongkang He <he@yongkang.cloud>
2023-05-12 16:54:36 +1000

    Create velero-destroy.sh

---
+++ velero-destroy.sh
@@ -0,0 +1,18 @@
+echo '-------Remove Velero for GKE and Clean up (typically in 3 mins)'
+starttime=$(date +%s)
+. ./setenv.sh
+
+velero uninstall --force
+gsutil rm -r gs://$(cat bucket4velero1) -f
+rm bucket4velero1
+rm yongsa4velero1
+gcloud iam service-accounts delete $(gcloud iam service-accounts list | grep vsa4yong1 | awk '{print $2}') -q
+
+endtime=$(date +%s)
+duration=$(( $endtime - $starttime ))
+echo "" | awk '{print $1}'
+echo "-------Total time to clean up the env is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "" | awk '{print $1}'
+echo "-------Created by Yongkang"
+echo "-------Email me if any suggestions or issues he@yongkang.cloud"
+echo "" | awk '{print $1}'
